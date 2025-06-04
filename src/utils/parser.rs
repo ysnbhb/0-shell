d70d76570db1ce07f1a21616db53ac27acb43ac2
ysnbhb <yassine.bahbib@usmba.ac.ma>
2025-06-04 16:18:35 +0100

    good

--- src/utils/parser.rs
+++ src/utils/parser.rs
@@ -6,17 +6,35 @@ pub fn naive_shell_split(input: &str) -> Result<Vec<String>, String> {
     let mut args = Vec::new();
     let mut current = String::new();
     let mut in_quotes = false;
-
+    let mut is_back = false;
     for c in input.chars() {
         match c {
-            '"' => in_quotes = !in_quotes,
+            '"' => {
+                if is_back {
+                    current.push('"');
+                    is_back = false;
+                    continue;
+                }
+                in_quotes = !in_quotes
+            }
             ' ' if !in_quotes => {
                 if !current.is_empty() {
                     args.push(current.clone());
                     current.clear();
                 }
             }
-            _ => current.push(c),
+            '\\' => {
+                if is_back {
+                    current.push('\\');
+                }
+                is_back = !is_back
+            }
+            _ => {
+                if is_back {
+                    current.push('\\');
+                }
+                current.push(c)
+            }
         }
     }
 
