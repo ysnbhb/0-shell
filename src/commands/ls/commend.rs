4117832fb52f11ead347f9becb5ca97e218d8524
oqritel <othmanqritel@gmail.com>
2025-07-14 17:32:38 +0100

    sort file

--- src/commands/ls/commend.rs
+++ src/commands/ls/commend.rs
@@ -1,13 +1,35 @@
-use crate::utils::fs::handle_flag;
+use std::{fs, path::Path};
+
+use crate::{commands::ls::print_ls::print_inside, utils::fs::handle_flag};
 
 pub fn ls(paths: &[String]) {
-    let res: Result<(bool, bool, bool, Vec<String>,bool), String> = handle_flag(paths);
+    let res: Result<(bool, bool, bool, Vec<String>, bool), String> = handle_flag(paths);
+
     match res {
         Ok(ref all) => {
             if all.3.len() == 0 {
                 return;
             }
-            println!("{:?}", res)
+            for i in all.3.clone() {
+                let mut paths = Vec::new();
+                if let Ok(entries) = fs::read_dir(&i) {
+                    for entry in entries {
+                        if let Ok(entry) = entry {
+                            if let Some(name) = entry.file_name().to_str() {
+                                if name.starts_with('.') && !all.0 {
+                                    continue;
+                                }
+                                paths.push(entry.path().as_os_str().to_string_lossy().to_string());
+                            }
+                        }
+                    }
+                    paths.sort();
+                    for path in paths {
+                        print_inside(Path::new(&path), all.1);
+                    }
+                    println!();
+                }
+            }
         }
         Err(e) => println!("{e}"),
     }
--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,8 @@ mod singals;
 pub mod commands;
 pub mod utils;
 
-
-use std::path::Path;
-
 use shell::*;
 
-use crate::commands::ls::permission::get_major_device_number;
-
-
 fn main() {
-    println!("{:?}",get_major_device_number(Path::new("/dev/vfio")))
+    shell();
 }
